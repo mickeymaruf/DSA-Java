98714f15820e2a34a0147753b73e47de761ac887
mickeymaruf <thegoldenpopo@gmail.com>
2022-06-14 16:22:28 +0600

    Hollow Rectangle

--- Patters.java
+++ Patters.java
@@ -1,17 +1,18 @@
 //Solid Rectangle
 
 // Expected Output:
-// *****
-// *****
-// *****
-// *****
+
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
 
 class Solid_Rectangle{
     public static void main(String args[]){
         String symbol = "*";
         for(int row = 1; row <= 4; row++){
             for(int col = 1; col <= 5; col++){
-                System.out.print(symbol);
+                System.out.print(symbol+" ");
             }
             System.out.println();
         }
@@ -21,13 +22,30 @@ class Solid_Rectangle{
 //Hollow Rectangle
 
 // Expected Output:
-// *****
-// *   *
-// *   *
-// *****
+
+// * * * * *
+// *       *
+// *       *
+// * * * * *
+
+// Not Clear !!!!!!!!!~~~~~~~~~~~~!!!!!!!!!!!~~~~~~~~~~~~
 
 class Hollow_Rectangle{
     public static void main(String args[]){
+        int n = 4;
+        int m = 5;
 
+        // outer loop
+        for(int row = 1; row <= n; row++){
+            // inner loop
+            for(int col =1; col <= m; col++){
+                if(row==1 || row==n || col==1 || col==n){
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
