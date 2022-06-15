b2a8db1a30418033b3f659e3f2f121df8f9771be
mickeymaruf <thegoldenpopo@gmail.com>
2022-06-15 23:05:03 +0600

    Inverted_Half_Pyramid_with_Numbers

--- Patters.java
+++ Patters.java
@@ -190,3 +190,29 @@ class Half_Pyramid_with_Numbers {
         }
     }
 }
+
+//Inverted Half Pyramid with Numbers
+
+// Expected Output:
+
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
+class Inverted_Half_Pyramid_with_Numbers {
+    public static void main(String args[]){
+        int n = 5;
+
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= n-i+1; j++){
+                System.out.print(j+" ");
+            }
+            // for(int j = 1; j <= i; j++){
+            //     System.out.print(j+" ");
+            // }
+            System.out.println();
+        }
+    }
+}
