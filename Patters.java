188c63bca80f28d22bf3fc1778288481bbc77803
mickeymaruf <thegoldenpopo@gmail.com>
2022-06-16 17:12:45 +0600

    0-1 Triangle

--- Patters.java
+++ Patters.java
@@ -242,3 +242,32 @@ class Floyds_Triangle {
         }
     }
 }
+
+
+//0-1 Triangle
+
+// Expected Output:
+
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+// 1 0 1 0 1
+
+class ZeroOne_Triangle {
+    public static void main(String args[]){
+        int n = 5;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                int sum = i + j;
+                if(sum % 2 == 0){ //even
+                    System.out.print(1+" ");
+                }else{ //odd
+                    System.out.print(0+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
